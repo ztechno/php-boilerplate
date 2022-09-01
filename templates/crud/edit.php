ddcf54683170b3b6f8baf1fb81276de3c2e5d0dc
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-09-01 11:23:18 +0700

    update config

--- templates/crud/edit.php
+++ templates/crud/edit.php
@@ -4,8 +4,8 @@
             <div class="page-inner py-5">
                 <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
                     <div>
-                        <h2 class="text-white pb-2 fw-bold">Edit <?=_ucwords($table)?> : <?=$data->nama??''?></h2>
-                        <h5 class="text-white op-7 mb-2">Memanajemen data <?=_ucwords($table)?></h5>
+                        <h2 class="text-white pb-2 fw-bold">Edit <?=_ucwords(__($table))?> : <?=$data->nama??''?></h2>
+                        <h5 class="text-white op-7 mb-2">Memanajemen data <?=_ucwords(__($table))?></h5>
                     </div>
                     <div class="ml-md-auto py-2 py-md-0">
                         <a href="<?=routeTo('crud/index',['table'=>$table])?>" class="btn btn-warning btn-round">Kembali</a>
--- templates/crud/index.php
+++ templates/crud/index.php
@@ -4,12 +4,12 @@
             <div class="page-inner py-5">
                 <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
                     <div>
-                        <h2 class="text-white pb-2 fw-bold"><?=_ucwords($table)?></h2>
-                        <h5 class="text-white op-7 mb-2">Memanajemen data <?=_ucwords($table)?></h5>
+                        <h2 class="text-white pb-2 fw-bold"><?=_ucwords(__($table))?></h2>
+                        <h5 class="text-white op-7 mb-2">Memanajemen data <?=_ucwords(__($table))?></h5>
                     </div>
                     <div class="ml-md-auto py-2 py-md-0">
                         <?php if(is_allowed(get_route_path('crud/create',['table'=>$table]),auth()->user->id)): ?>
-                            <a href="<?=routeTo('crud/create',['table'=>$table])?>" class="btn btn-secondary btn-round">Buat <?=_ucwords($table)?></a>
+                            <a href="<?=routeTo('crud/create',['table'=>$table])?>" class="btn btn-secondary btn-round">Buat <?=_ucwords(__($table))?></a>
                         <?php endif ?>
                     </div>
                 </div>
