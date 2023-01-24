7617cd5aa6ed894099e9e11cee437eee496250c1
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-01-25 04:58:33 +0700

    remove migration from init

--- init.php
+++ init.php
@@ -15,17 +15,3 @@ foreach($files as $file)
 
 echo "Init Config File Success\n";
 echo "Please config the following file in config folder (".(implode('.php, ',$files)).")\n";
-
-$files = [
-    'init'
-];
-
-foreach($files as $file)
-{
-    if(!file_exists('migrations/'.$file.'.php'))
-    {
-        copy('migrations/'.$file.'.example','migrations/'.$file.'.sql');
-    }
-}
-
-echo "Init Migration File Success\n";
